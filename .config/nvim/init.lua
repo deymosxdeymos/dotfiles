54c6360888788482bee434a5bcbee92713cf6024
deymosxdeymos <galinnichola15@gmail.com>
2025-10-13 18:59:58 +0700

    Sync local config tweaks

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,128 +1,293 @@
 vim.cmd([[set mouse=]])
 vim.cmd([[set noswapfile]])
+
 vim.opt.winborder = "rounded"
-vim.opt.tabstop = 4
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.showtabline = 2
+vim.opt.signcolumn = "yes"
 vim.opt.wrap = false
 vim.opt.cursorcolumn = false
 vim.opt.ignorecase = true
-vim.opt.shiftwidth = 4
 vim.opt.smartindent = true
-vim.opt.number = true
-vim.opt.relativenumber = true
 vim.opt.termguicolors = true
 vim.opt.undofile = true
-vim.opt.signcolumn = "yes"
+vim.opt.number = true
 vim.opt.clipboard = "unnamedplus"
 
 vim.pack.add({
-  { src = "https://github.com/ellisonleao/gruvbox.nvim" },
-  { src = "https://github.com/datsfilipe/vesper.nvim" },
-  { src = "https://github.com/stevearc/oil.nvim" },
-  { src = "https://github.com/echasnovski/mini.nvim" },
-  { src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "main" },
-  { src = 'https://github.com/neovim/nvim-lspconfig' },
-  { src = "https://github.com/mason-org/mason.nvim" },
-  { src = "https://github.com/L3MON4D3/LuaSnip" },
-  { src = "https://github.com/wakatime/vim-wakatime" },
-}, { load = true })
+    { src = "https://github.com/ellisonleao/gruvbox.nvim" },
+    { src = "https://github.com/chentoast/marks.nvim" },
+    { src = "https://github.com/stevearc/oil.nvim" },
+    { src = "https://github.com/nvim-tree/nvim-web-devicons" },
+    { src = "https://github.com/aznhe21/actions-preview.nvim" },
+    { src = "https://github.com/nvim-treesitter/nvim-treesitter",        version = "main" },
+    { src = "https://github.com/nvim-telescope/telescope.nvim",          version = "0.1.8" },
+    { src = "https://github.com/nvim-lua/plenary.nvim" },
+    { src = "https://github.com/chomosuke/typst-preview.nvim" },
+    { src = "https://github.com/neovim/nvim-lspconfig" },
+    { src = "https://github.com/mason-org/mason.nvim" },
+    { src = "https://github.com/L3MON4D3/LuaSnip" },
+    { src = "https://github.com/nvim-telescope/telescope-ui-select.nvim" },
+    { src = "https://github.com/LinArcX/telescope-env.nvim" },
+    { src = "https://github.com/wakatime/vim-wakatime" },
+})
+
+require "marks".setup {
+    builtin_marks = { "<", ">", "^" },
+    refresh_interval = 250,
+    sign_priority = { lower = 10, upper = 15, builtin = 8, bookmark = 20 },
+    excluded_filetypes = {},
+    excluded_buftypes = {},
+    mappings = {}
+}
+
+
+local default_color = "gruvbox"
 
 require "mason".setup()
-require "mini.pick".setup()
-require "mini.pairs".setup()
-require "mini.icons".setup()
-require "mini.statusline".setup()
-require "mini.bufremove".setup()
-require "oil".setup()
+require "telescope".setup({
+    defaults = {
+        preview = { treesitter = false },
+        color_devicons = true,
+        sorting_strategy = "ascending",
+        borderchars = { "", "", "", "", "", "", "", "" },
+        path_displays = { "smart" },
+        layout_config = {
+            height = 100,
+            width = 400,
+            prompt_position = "top",
+            preview_cutoff = 40,
+        }
+    }
+})
+
+require("actions-preview").setup {
+    backend = { "telescope" },
+    extensions = { "env" },
+    telescope = vim.tbl_extend(
+        "force",
+        require("telescope.themes").get_dropdown(), {}
+    )
+}
+
+-- Some servers (e.g. Tailwind CSS) produce huge completion lists; keep them manual to avoid lag.
+local manual_completion_clients = {
+    tailwindcss = true,
+}
 
 vim.api.nvim_create_autocmd('LspAttach', {
-  group = vim.api.nvim_create_augroup('my.lsp', {}),
-  callback = function(args)
-    local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
-    if client:supports_method('textDocument/completion') then
-      vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
-    end
-    -- Use Biome for formatting TypeScript/JavaScript files
-    if client.name == "biome" and client:supports_method('textDocument/formatting') then
-      vim.bo[args.buf].formatexpr = 'v:lua.vim.lsp.formatexpr()'
-    end
-    -- Disable ts_ls formatting when Biome is available
-    if client.name == "ts_ls" then
-      client.server_capabilities.documentFormattingProvider = false
-      client.server_capabilities.documentRangeFormattingProvider = false
-    end
-  end,
+    group = vim.api.nvim_create_augroup('my.lsp', {}),
+    callback = function(args)
+        local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+        if client:supports_method('textDocument/completion') then
+            local autotrigger = not manual_completion_clients[client.name]
+            vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = autotrigger })
+        end
+    end,
 })
-
-vim.diagnostic.config({
-  virtual_text = {
-    severity = {
-      max = vim.diagnostic.severity.WARN,
-    },
-  },
-  virtual_lines = {
-    severity = {
-      min = vim.diagnostic.severity.ERROR,
-    },
-  },
-})
-
-
--- lsp
-vim.lsp.enable(
-  {
-    "lua_ls",
-    "svelte",
-    "ts_ls",
-    "emmet_ls",
-    "rust_analyzer",
-    "clangd",
-    "ruff",
-    "tailwindcss",
-    "biome",
-  }
-)
 vim.cmd [[set completeopt+=menuone,noselect,popup]]
 
--- colors
+
+vim.lsp.enable({
+    "lua_ls", "cssls", "svelte", "tinymist",
+    "rust_analyzer", "clangd", "ruff",
+    "glsl_analyzer", "haskell-language-server", "hlint",
+    "intelephense", "biome", "tailwindcss",
+    "ts_ls", "emmet_language_server"
+})
+
+require("oil").setup({
+    lsp_file_methods = {
+        enabled = true,
+        timeout_ms = 1000,
+        autosave_changes = true,
+    },
+    columns = {
+        "permissions",
+        "icon",
+    },
+    float = {
+        max_width = 0.7,
+        max_height = 0.6,
+        border = "rounded",
+    },
+})
+
 require("gruvbox").setup({
-  contrast = "hard",
+    contrast = "hard",
 })
 vim.o.background = "dark"
-vim.cmd("colorscheme gruvbox")
--- vim.cmd(":hi statusline guibg=NONE")
 
--- snippets
 require("luasnip").setup({ enable_autosnippets = true })
 require("luasnip.loaders.from_lua").load({ paths = "~/.config/nvim/snippets/" })
-local ls = require("luasnip")
 
--- mappings
+_G.pack_clean = function()
+    local active_plugins = {}
+    local unused_plugins = {}
+
+    for _, plugin in ipairs(vim.pack.get()) do
+        active_plugins[plugin.spec.name] = plugin.active
+    end
+
+    for _, plugin in ipairs(vim.pack.get()) do
+        if not active_plugins[plugin.spec.name] then
+            table.insert(unused_plugins, plugin.spec.name)
+        end
+    end
+
+    if #unused_plugins == 0 then
+        print("No unused plugins.")
+        return
+    end
+
+    local choice = vim.fn.confirm("Remove unused plugins?", "&Yes\n&No", 2)
+    if choice == 1 then
+        vim.pack.del(unused_plugins)
+    end
+end
+
+vim.keymap.set("n", "<leader>pc", _G.pack_clean)
+
+
+local color_group = vim.api.nvim_create_augroup("colors", { clear = true })
+
+vim.api.nvim_create_autocmd("ColorScheme", {
+    group = color_group,
+    callback = function(args)
+        if vim.t.color then
+            vim.cmd("colorscheme " .. vim.t.color)
+        else
+            vim.cmd("colorscheme " .. default_color)
+        end
+    end,
+})
+
+vim.api.nvim_create_autocmd("TabEnter", {
+    group = color_group,
+    callback = function(args)
+        if vim.t.color then
+            vim.cmd("colorscheme " .. vim.t.color)
+        else
+            vim.cmd("colorscheme " .. default_color)
+        end
+    end,
+})
+
+local colors = {}
+vim.api.nvim_create_autocmd("ColorScheme", {
+    group = color_group,
+    callback = function(args)
+        -- vim.cmd("hi statusline guibg=NONE")
+        -- vim.cmd("hi TabLineFill guibg=NONE")
+    end,
+})
+
+local ls = require("luasnip")
+local builtin = require("telescope.builtin")
 local map = vim.keymap.set
+local current = 1
+
+function random_theme()
+    local colors = vim.fn.getcompletion("", "color")
+    if current < #colors then
+        current = current + 1
+    else
+        current = 1
+    end
+    local color = colors[current]
+    vim.t.color = color
+    vim.cmd("colorscheme " .. color)
+end
+
 vim.g.mapleader = " "
-map('n', '<leader>w', '<Cmd>write<CR>')
-map('n', '<leader>q', require("mini.bufremove").delete)
-map('n', '<C-f>', '<Cmd>Open .<CR>')
-map('n', '<leader>v', '<Cmd>e $MYVIMRC<CR>')
-map({ 'n', 'v' }, '<leader>n', ':norm ')
-map({ 'n', 'v' }, '<leader>y', '"+y')
-map({ 'n', 'v' }, '<leader>d', '"+d')
-map({ 'n', 'v' }, '<leader>c', '1z=')
-map({ 'n', 'v' }, '<leader>o', ':update<CR> :source<CR>')
-map('t', '', "")
-map('t', '', "")
-map('n', '<leader>lf', vim.lsp.buf.format)
-map("i", "<C-e>", function() ls.expand_or_jump(1) end, { silent = true })
+map({ "n" }, "<leader>m", function() random_theme() end)
+map({ "n", "x" }, "<leader>y", '"+y')
+map({ "n", "x" }, "<leader>d", '"+d')
+map({ "i", "s" }, "<C-e>", function() ls.expand_or_jump(1) end, { silent = true })
 map({ "i", "s" }, "<C-J>", function() ls.jump(1) end, { silent = true })
 map({ "i", "s" }, "<C-K>", function() ls.jump(-1) end, { silent = true })
-map('n', '<leader>f', "<Cmd>Pick files<CR>")
-map('n', '<leader>r', "<Cmd>Pick buffers<CR>")
-map('n', '<leader>h', "<Cmd>Pick help<CR>")
-map('n', '<leader>/', "<Cmd>Pick grep_live<CR>")
-map('n', '-', "<Cmd>Oil<CR>")
-map('i', '<c-e>', function() vim.lsp.completion.get() end)
-map("n", "<C-d>", "<C-d>zz", opts)
-map("n", "<C-u>", "<C-u>zz", opts)
-map("v", "J", ":m '>+1<CR>gv=gv", opts)
-map("v", "K", ":m '<-2<CR>gv=gv", opts)
-map("n", "j", "gj", opts)
-map("n", "k", "gk", opts)
+map({ "n", "t" }, "<Leader>t", "<Cmd>tabnew<CR>")
+map({ "n", "t" }, "<Leader>x", "<Cmd>tabclose<CR>")
+
+
+vim.cmd([[
+    nnoremap g= g+| " g=g=g= is less awkward than g+g+g+
+    nnoremap gK @='ddkPJ'<cr>| " join lines but reversed. `@=` so [count] works
+    xnoremap gK <esc><cmd>keeppatterns '<,'>-global/$/normal! ddpkJ<cr>
+    noremap! <c-r><c-d> <c-r>=strftime('%F')<cr>
+    noremap! <c-r><c-t> <c-r>=strftime('%T')<cr>
+    noremap! <c-r><c-f> <c-r>=expand('%:t')<cr>
+    noremap! <c-r><c-p> <c-r>=expand('%:p')<cr>
+    xnoremap <expr> . "<esc><cmd>'<,'>normal! ".v:count1.'.<cr>'
+]])
+
+for i = 1, 8 do
+    map({ "n", "t" }, "<Leader>" .. i, "<Cmd>tabnext " .. i .. "<CR>")
+end
+map({ "n", "v", "x" }, ";", ":", { desc = "Self explanatory" })
+map({ "n", "v", "x" }, ":", ";", { desc = "Self explanatory" })
+map({ "n", "v", "x" }, "<leader>v", "<Cmd>edit $MYVIMRC<CR>", { desc = "Edit " .. vim.fn.expand("$MYVIMRC") })
+map({ "n", "v", "x" }, "<leader>z", "<Cmd>e ~/.config/zsh/.zshrc<CR>", { desc = "Edit .zshrc" })
+map({ "n", "v", "x" }, "<leader>n", ":norm ", { desc = "ENTER NORM COMMAND." })
+map({ "n", "v", "x" }, "<leader>o", "<Cmd>source %<CR>", { desc = "Source " .. vim.fn.expand("$MYVIMRC") })
+map({ "n", "v", "x" }, "<leader>O", "<Cmd>restart<CR>", { desc = "Restart vim." })
+map({ "n", "v", "x" }, "<C-s>", [[:s/\V]], { desc = "Enter substitue mode in selection" })
+map({ "n", "v", "x" }, "<leader>lf", vim.lsp.buf.format, { desc = "Format current buffer" })
+map({ "v", "x", "n" }, "<C-y>", '"+y', { desc = "System clipboard yank." })
+map({ "n" }, "<leader>f", builtin.find_files, { desc = "Telescope live grep" })
+map({ "n" }, "<leader>g", builtin.live_grep, { desc = "Telescope live grep" })
+map({ "n" }, "<leader>b", builtin.buffers, { desc = "Telescope buffers" })
+map({ "n" }, "<leader>si", builtin.grep_string, { desc = "Telescope live string" })
+map({ "n" }, "<leader>so", builtin.oldfiles, { desc = "Telescope buffers" })
+map({ "n" }, "<leader>sh", builtin.help_tags, { desc = "Telescope help tags" })
+map({ "n" }, "<leader>sm", builtin.man_pages, { desc = "Telescope man pages" })
+map({ "n" }, "<leader>sr", builtin.lsp_references, { desc = "Telescope tags" })
+map({ "n" }, "<leader>st", builtin.builtin, { desc = "Telescope tags" })
+map({ "n" }, "<leader>sd", builtin.registers, { desc = "Telescope tags" })
+map({ "n" }, "<leader>sc", builtin.colorscheme, { desc = "Telescope tags" })
+map({ "n" }, "<leader>se", "<cmd>Telescope env<cr>", { desc = "Telescope tags" })
+map({ "n" }, "<leader>sa", require("actions-preview").code_actions)
+map({ "n" }, "<M-n>", "<cmd>resize +2<CR>")
+map({ "n" }, "<M-e>", "<cmd>resize -2<CR>")
+map({ "n" }, "<M-i>", "<cmd>vertical resize +5<CR>")
+map({ "n" }, "<M-m>", "<cmd>vertical resize -5<CR>")
+map({ "n" }, "<leader>e", "<cmd>Oil<CR>")
+map({ "n" }, "<leader>c", "1z=")
+map({ "n" }, "<C-q>", ":copen<CR>", { silent = true })
+map({ "n" }, "<leader>w", "<Cmd>update<CR>", { desc = "Write the current buffer." })
+map({ "n" }, "<leader>q", "<Cmd>:quit<CR>", { desc = "Quit the current buffer." })
+map({ "n" }, "<leader>Q", "<Cmd>:wqa<CR>", { desc = "Quit all buffers and write." })
+map({ "n" }, "<C-f>", "<Cmd>Open .<CR>", { desc = "Open current directory in Finder." })
+map({ "n" }, "<leader>a", ":edit #<CR>", { desc = "Open current directory in Finder." })
+
+
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+vim.api.nvim_create_autocmd("BufWinEnter", {
+    pattern = "*.jsx,*.tsx",
+    group = vim.api.nvim_create_augroup("TS", { clear = true }),
+    callback = function()
+        vim.cmd([[set filetype=typescriptreact]])
+    end
+})
+vim.cmd('colorscheme ' .. default_color)
+
+-- Run gg-repo-sync automatically after saving a PHP file
+vim.api.nvim_create_autocmd("BufWritePost", {
+  pattern = "*.php",
+  callback = function()
+    vim.fn.jobstart("gg-repo-sync", {
+      on_exit = function(_, exit_code, _)
+        if exit_code == 0 then
+          vim.notify("gg-repo-sync successful", vim.log.levels.INFO)
+        else
+          vim.notify("gg-repo-sync failed", vim.log.levels.ERROR)
+        end
+      end,
+    })
+  end,
+})
